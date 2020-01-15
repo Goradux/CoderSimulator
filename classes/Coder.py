03a81a355d3133c48a55a3623b20725d241712e8
Goradux <abbbo@mail.ru>
2020-01-15 16:54:18 +0100

    added some printing functions

--- classes/Coder.py
+++ classes/Coder.py
@@ -4,7 +4,8 @@ import sys, time, random
 # use it like this: stack = deque(); stack.append(var); var = stack.pop();
 from collections import deque
 
-list_of_vars = ['x', 'y', 'i', 'index', 'foo', 'bar', 'foobar', 'amount', 'width', 'height']
+list_of_vars = ['x', '_', 'y', 'i', 'index', 'foo', 'bar', 'foobar', 'amount', 'width', 'height']
+list_of_functions = ['add', 'remove']
 
 class Coder:
 
@@ -13,8 +14,10 @@ class Coder:
     tab = 0
     vars_in_use = []
     buffered_actions = deque()
+    followup = False
 
     def humanoid_print(self, print_out):
+        # print(self.tab)
         for _ in range(self.tab):
                 print('    ', end = '')
         for index in range(len(print_out)):
@@ -23,53 +26,131 @@ class Coder:
             time.sleep(random.randint(1, 10)/100)
             if index == len(print_out) - 1:
                 print()
+        # time.sleep(0.05)
+        time.sleep(1 if random.randint(0, 10) == 0 else 0)
 
     def roll_indent(self):
         # random roll for a chance to indent 1 level back
-        pass
+        if self.tab > 0 and self.followup == False:
+            roll = random.randint(0, 99)
+            if roll < 50:
+                self.tab = self.tab - 1
+                if len(self.buffered_actions) > 0:
+                    self.buffered_actions.pop()()
+                    self.followup = True
+                    # deindent = self.buffered_actions.pop()
+                    # print(deindent)
+            elif roll < 85:
+
+                #BUG THIS GOT BROKEN
+                if len(self.buffered_actions) == 0:
+                    self.tab = self.tab - 2
+                    # self.buffered_actions.pop()()
+                    # self.followup = True
+                    # deindent = self.buffered_actions.pop()
+                    # print(deindent)
+            self.tab = 0 if self.tab < 0 else self.tab 
+        # 25% chance to go back one level
+        # # 10% chance to go back two levels
+
+    def roll_space(self):
+        roll = random.randint(0, 99)
+        if roll < 15:
+            print()
 
     def if_statement(self):
-        self.humanoid_print('if')
+        self.humanoid_print('if:')
+        self.tab = self.tab + 1
+        self.followup = True
 
     def else_statement(self):
-        self.humanoid_print('else')
+        self.humanoid_print('else:')
+        self.tab = self.tab + 1
+        self.followup = True
 
     def if_else_statement(self):
-        # self.if_statement()
-        self.humanoid_print('if else')
+        self.if_statement()
+        # self.humanoid_print('if else')
+
+    def if_inline_statement(self):
+        self.humanoid_print(random.choice(list_of_vars) + ' = ' + 'statement if statement else 0')
+        self.followup = False
+
 
     def try_statement(self):
-        self.humanoid_print('try')
+        self.humanoid_print('try:')
+        self.tab = self.tab + 1
+        self.followup = True
 
     def except_statement(self):
-        self.humanoid_print('except')
+        if random.randint(0, 1) is 0:
+            self.humanoid_print('except:')
+        else:
+            self.humanoid_print('except Exception as e:')
+        self.tab = self.tab + 1
+        self.followup = True
 
     def create_loop(self):
-        start = 'for ' + random.choice(self.vars_in_use) + ' in range(0, 10):'
-        self.humanoid_print(start)
+        loop = 'for ' + random.choice(list_of_vars) + ' in range(' + str(random.randint(0, 50)) + ', ' + str(random.randint(50, 250)) + '):'
+        self.humanoid_print(loop)
         self.tab = self.tab + 1
+        self.followup = True
 
     def create_function(self):
-        self.humanoid_print('function')
+        self.humanoid_print('function:')
+        self.tab = self.tab + 1
+        self.followup = True
 
-    def assign_var(self):
+    def assign_var_value(self):
         random_var = random.choice(list_of_vars)
         self.vars_in_use.append(random_var)
         output = random_var + ' = ' + str(random.randint(0, 10))
         self.humanoid_print(output)
+        self.followup = False
 
-    action_choices = [assign_var, create_loop, create_function, if_statement,
-        if_else_statement]
+    
+    def print_statement(self):
+        self.humanoid_print('print(' + random.choice(list_of_vars) + ')')
+        self.followup = False
+    
+    def execute_function_statement(self):
+        self.humanoid_print(random.choice(list_of_functions) + '(' + random.choice(list_of_vars) +')')
+        self.followup = False
 
+    def comment_statement(self):
+        comment = '# some comment'
+        self.humanoid_print(comment)
+
+    # def pass_statement(self):
+    #     self.humanoid_print('pass')
+
+    action_choices = [if_else_statement, try_statement, create_loop, create_function,
+        if_statement, if_inline_statement, print_statement, assign_var_value,
+        execute_function_statement, comment_statement]
+    action_choices_simple = [assign_var_value, print_statement, execute_function_statement,
+        comment_statement]
     def choose_action(self):
         self.next_action = random.choice(self.action_choices)
+        # print(self.next_action)
+        self.next_action(self)
         
         # these two actions are composite, and require a second part with
         # indentation back and forth
-        if self.next_action == self.if_else_statement:
+        # NOTE: compared to the part of the action_choices and not the
+        # functions themselves because the memory slots are different
+        
+        # at least one statement is required so that it wont indent back on empty line
+
+        # if_else_statement
+        if self.next_action == self.action_choices[0]:
             self.buffered_actions.append(self.else_statement)
-        if self.next_action == self.try_statement:
+            self.next_action = random.choice(self.action_choices_simple)
+            self.next_action(self)
+        # try_statement
+        if self.next_action == self.action_choices[1]:
             self.buffered_actions.append(self.except_statement)
+            self.next_action = random.choice(self.action_choices_simple)
+            self.next_action(self)
 
     def action_cleanup(self):
         self.next_action = None
@@ -77,22 +158,14 @@ class Coder:
 
     def do_next_action(self):
         self.choose_action()
-        self.next_action(self)
-
-        # self.buffered_actions.pop()()             #execute else: or except:
-
-        # if self.buffered_action is not None:
-        #     self.tab = self.tab - 1
-        #     self.buffered_action()
+        self.roll_indent()
+        self.roll_space()
         self.action_cleanup()
 
         self.do_next_action()
 
-    def __init__(self):
-        print('Coder init done')
-        # self.humanoid_print('something')
-        # self.create_loop()
-        # self.assign_var()
+    # def __init__(self):
+    #     print('Coder init done')
 
     def start(self):
         self.do_next_action()
