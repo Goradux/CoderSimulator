882bec3ef26d166da864196f5f1fd685c6502a38
Goradux <abbbo@mail.ru>
2020-01-17 16:44:44 +0100

    Added more logic, pretty much only value generators and colors are left

--- classes/Coder.py
+++ classes/Coder.py
@@ -34,7 +34,7 @@ class Coder:
             if index == len(print_out) - 1:
                 print()
         # time.sleep(0.05)
-        time.sleep(1 if random.randint(0, 10) == 0 else 0)
+        # time.sleep(1 if random.randint(0, 10) == 0 else 0)
 
     def roll_indent(self):
         # random roll for a chance to indent 1 level back
@@ -65,8 +65,21 @@ class Coder:
         if roll < 15:
             print()
 
+    def generate_statement(self):
+        return '(STATEMENT)'
+
+    def generate_value(self):
+        return '(VALUE)'
+
+    def generate_iterable(self):
+        iterable = 'range(' + str(random.randint(0, 50)) + ', ' + str(random.randint(50, 250)) + ')'
+        return '(ITERABLE)'
+    
+    def generate_function_name(self):
+        return '(FUNCTION NAME)'
+
     def if_statement(self):
-        self.humanoid_print('if (STATEMENT):')
+        self.humanoid_print('if ' + self.generate_statement() + ':')
         self.tab = self.tab + 1
         self.followup = True
 
@@ -80,9 +93,8 @@ class Coder:
         # self.humanoid_print('if else')
 
     def if_inline_statement(self):
-        self.humanoid_print(random.choice(list_of_vars) + ' = ' + '(STATEMENT) if (STATEMENT) else 0')
+        self.humanoid_print(random.choice(list_of_vars) + ' = ' + '(VALUE) if (STATEMENT) else (VALUE)')
         self.followup = False
-
 
     def try_statement(self):
         self.humanoid_print('try:')
@@ -93,31 +105,30 @@ class Coder:
         if random.randint(0, 1) is 0:
             self.humanoid_print('except:')
         else:
-            self.humanoid_print('except Exception as e:')
+            self.humanoid_print('except (EXCEPTION) as e:')
         self.tab = self.tab + 1
         self.followup = True
 
     def create_loop(self):
-        loop = 'for ' + random.choice(list_of_vars) + ' in range(' + str(random.randint(0, 50)) + ', ' + str(random.randint(50, 250)) + '):'
+        loop = 'for ' + random.choice(list_of_vars) + ' in ' + self.generate_iterable() + ':'
         self.humanoid_print(loop)
         self.tab = self.tab + 1
         self.followup = True
 
     def create_function(self):
-        self.humanoid_print('function (FUNCTION):')
+        self.humanoid_print('def ' + self.generate_function_name() + ':')
         self.tab = self.tab + 1
         self.followup = True
 
     def assign_var_value(self):
         random_var = random.choice(list_of_vars)
-        self.vars_in_use.append(random_var)
-        output = random_var + ' = ' + str(random.randint(0, 10))
+        # self.vars_in_use.append(random_var)
+        output = random_var + ' = ' + self.generate_value()
         self.humanoid_print(output)
         self.followup = False
-
     
     def print_statement(self):
-        self.humanoid_print('print(' + random.choice(list_of_vars) + ')')
+        self.humanoid_print('print(' + self.generate_value() + ')')
         self.followup = False
     
     def execute_function_statement(self):
@@ -131,17 +142,41 @@ class Coder:
     # def pass_statement(self):
     #     self.humanoid_print('pass')
 
+    def code_block(self, size=None):
+        if size is None:
+            size = 3
+        
+        for _ in range(size):
+            self.humanoid_print('a line of a code_block')
+        
+        print()
+
+    # def code_block_bigger(self):
+    #     self.code_block(5)
+    #     pass
+
+    # def code_block_biggest(self):
+    #     self.code_block(7)
+    #     pass
+
+    # a set of all possible actions
     action_choices = [if_else_statement, try_statement, create_loop, create_function,
         if_statement, if_inline_statement, print_statement, assign_var_value,
-        execute_function_statement, comment_statement]
+        execute_function_statement, comment_statement, code_block]
+    # a set of actions that do not require indentation
     action_choices_simple = [assign_var_value, print_statement, execute_function_statement,
-        comment_statement]
+        comment_statement, code_block]
     def choose_action(self):
+        
+        # choose the next action
         self.next_action = random.choice(self.action_choices) if self.tab < 5 else random.choice(self.action_choices_simple)
-        # print(self.next_action)
+        
+        #perform the next action
         self.next_action(self)
         
-        # these two actions are composite, and require a second part with
+
+
+        # these following two actions are composite, and require a second part with
         # indentation back and forth
         # NOTE: compared to the part of the action_choices and not the
         # functions themselves because the memory slots are different
@@ -163,6 +198,7 @@ class Coder:
         self.next_action = None
         # self.buffered_action = None
 
+    # the main action loop, recursive
     def do_next_action(self):
         self.choose_action()
         self.roll_indent()
